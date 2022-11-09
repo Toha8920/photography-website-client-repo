5032b4cc6e0fa39013060523860974914e158940
Toha Islam <islamtoha710@gmail.com>
2022-11-09 11:04:18 +0600

    added environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBuFDSZO-Z8ZV0Y2LE1eWUr4E6hk3yHoYA",
-    authDomain: "photography-a5d3a.firebaseapp.com",
-    projectId: "photography-a5d3a",
-    storageBucket: "photography-a5d3a.appspot.com",
-    messagingSenderId: "337754774908",
-    appId: "1:337754774908:web:8de854ee7e2927328698f9"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
